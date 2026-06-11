770f8b9834da87b26b9b81beda62c79f4a460e86
Aaril <arilansaribiz@gmail.com>
2026-06-11 20:21:56 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Aaril Ansari</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quis, ab.</p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default App;
